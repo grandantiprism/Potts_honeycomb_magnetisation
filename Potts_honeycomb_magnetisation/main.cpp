7e181993b0101ec0e700ecad1abc77d9a30fe670
ADE <ackielilac.g1@gmail.com>
2026-01-12 00:41:32 +0900

    Initial Commit

---
+++ Potts_honeycomb_magnetisation/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Potts_honeycomb_magnetisation
+//
+//  Created by Akihisa Tateishi on 2026/01/12.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
